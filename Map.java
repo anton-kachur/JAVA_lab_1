70e025a0e10bbf996359b3c201c1c6d09b400de9
anton-kachur <40312443+anton-kachur@users.noreply.github.com>
2020-10-21 14:57:16 +0300

    Update Map.java

--- Map.java
+++ Map.java
@@ -23,3 +23,77 @@ private static class Map
             }
 
         }
+        //***********************ArrayLists which contain map components**************************************
+        private ArrayList<Road> roads = new ArrayList<>();
+        private ArrayList<Reservoir> reservoirs = new ArrayList<>();
+        private ArrayList<Building> buildings = new ArrayList<>();
+        private ArrayList<Locality> localities = new ArrayList<>();
+        private ArrayList<Trees> trees = new ArrayList<>();
+        private ArrayList<Relief> reliefs = new ArrayList<>();
+
+        //*************Function, that represents the list of localities on alphabetical order*****************
+        private void listOfLocalities() {
+            if (localities.size() < 1) {
+                System.out.println("\nThere is no localities\n");
+            } else {
+                Collections.sort(localities, Comparator.comparing(Locality::getName));
+                System.out.println("All localities in alphabetic order: ");
+
+                for (int i=0; i<localities.size(); i++)
+                    System.out.println(localities.get(i).locality_name);
+            }
+        }
+
+
+        //**************Methods for setting map components and representing them*****************************
+        private void printMap() {
+            for (Road i: roads) i.printAll();
+            for (Reservoir i: reservoirs) i.printAll();
+            for (Building i: buildings) i.printAll();
+            for (Locality i: localities) i.printAll();
+            for (Trees i: trees) i.printAll();
+            for (Relief i: reliefs) i.printAll();
+        }
+
+        private void setNewRoad(int amount) {
+            for (int i=0; i<amount; i++) {
+                Random random = new Random();
+                roads.add(new Road(road_types[random.nextInt(road_types.length)], random.nextInt(7)));
+            }
+        }
+
+        private void setNewReservoir(int amount) {
+            for (int i=0; i<amount; i++) {
+                Random random = new Random();
+                reservoirs.add(new Reservoir(reservoir_types[random.nextInt(reservoir_types.length)], random.nextInt(1000)));
+            }
+        }
+
+        private void setNewBuilding(int amount) {
+            for (int i=0; i<amount; i++) {
+                buildings.add(new Building("SomeBuilding", "SomeLocality"));
+            }
+        }
+
+        private void setNewLocality(int amount) {
+            for (int i=0; i<amount; i++) {
+                Random random = new Random();
+                localities.add(new Locality(generateName(), locality_types[random.nextInt(locality_types.length)]));
+            }
+        }
+
+        private void setNewTree(int amount) {
+            for (int i=0; i<amount; i++) {
+                Random random = new Random();
+                trees.add(new Trees("Something", trees_types[random.nextInt(trees_types.length)]));
+            }
+        }
+
+        private void setNewRelief(int amount) {
+            for (int i=0; i<amount; i++) {
+                Random random = new Random();
+                reliefs.add(new Relief(random.nextInt(5000)));
+            }
+        }
+
+    }
