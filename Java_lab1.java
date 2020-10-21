d56277b2825a1f6bc7cbb94186eae8c3debb5db1
anton-kachur <40312443+anton-kachur@users.noreply.github.com>
2020-10-21 15:05:16 +0300

    Update Java_lab1.java

--- Java_lab1.java
+++ Java_lab1.java
@@ -62,25 +62,7 @@ public class Java_lab1 {
     //*************************************************************************************************
     //**************************Creates application****************************************************
     //*************************************************************************************************
-    private static class Application
-    {
-        private void makeMap(Choice choice)
-        {
-            Director director = new Director();
-            MapBuilder builder = new MapBuilder();
-
-            switch(choice) {
-                case RANDOM: director.createDefaultMap(builder); break;
-                case CUSTOM: director.createUserMap(builder); break;
-            }
-
-            Map map = builder.resultMap();
-            map.printMap();
-            map.listOfLocalities();
-        }
-
-    }
-
+    
 
     public enum Choice {
         RANDOM,
