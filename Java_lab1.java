deb0025067b19e89d0090293acdf890a952ee743
anton-kachur <40312443+anton-kachur@users.noreply.github.com>
2020-10-21 14:50:49 +0300

    Update Java_lab1.java

--- Java_lab1.java
+++ Java_lab1.java
@@ -34,24 +34,6 @@ public class Java_lab1 {
     //*******************All necessary classes*********************************************************
     //*************************************************************************************************
 
-    public static class Locality extends Building
-    {
-        private String locality_name;
-        private String locality_type;
-
-        private Locality(String name, String type) {
-            super("Default name", name);
-
-            locality_name = name;
-            locality_type = type;
-        }
-
-        private String getName() { return locality_name; }
-
-        public void printAll() { System.out.println("Name of locality: " + locality_name + "\nType of locality: " + locality_type); }
-    }
-
-
     private static class Trees
     {
         private String name;
