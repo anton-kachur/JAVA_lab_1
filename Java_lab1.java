d57726490fe5e9e30dcab2be37e0f091a7abca59
anton-kachur <40312443+anton-kachur@users.noreply.github.com>
2020-10-21 14:51:35 +0300

    Update Java_lab1.java

--- Java_lab1.java
+++ Java_lab1.java
@@ -33,21 +33,7 @@ public class Java_lab1 {
     //*************************************************************************************************
     //*******************All necessary classes*********************************************************
     //*************************************************************************************************
-
-    private static class Trees
-    {
-        private String name;
-        private String type;
-
-        private Trees(String name, String type) {
-            this.name = name;
-            this.type = type;
-        }
-
-        private void printAll() {
-            System.out.println("Name: " + name + "\nType: " + type);
-        }
-    }
+    
 
 
     //*************************************************************************************************
