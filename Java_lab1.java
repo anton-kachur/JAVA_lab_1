aa44c235d98fcd1e57372297720d4f92b649a3e8
anton-kachur <40312443+anton-kachur@users.noreply.github.com>
2020-10-21 14:57:48 +0300

    Update Java_lab1.java

--- Java_lab1.java
+++ Java_lab1.java
@@ -45,21 +45,7 @@ public class Java_lab1 {
     //*************************************************************************************************
     //***************Class which contains all functions for map generating*****************************
     //*************************************************************************************************
-    public static abstract class Builder
-    {
-        public abstract void setRoads();
-        public abstract void setReservoirs();
-        public abstract void setBuildings();
-        public abstract void setLocalities();
-        public abstract void setLocalities(int number_of_localities);
-        public abstract void setTrees();
-        public abstract void setRelief();
-        public abstract void setRoads(int n);
-        public abstract void setReservoirs(int n);
-        public abstract void setBuildings(int n);
-        public abstract void setTrees(int n);
-        public abstract void setRelief(int n);
-    }
+    
 
 
     //*************************************************************************************************
