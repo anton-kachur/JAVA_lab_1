27bff8b0ccec63ec95aa841a096a626fb7bb48d2
anton-kachur <40312443+anton-kachur@users.noreply.github.com>
2020-10-21 15:02:35 +0300

    Update Java_lab1.java

--- Java_lab1.java
+++ Java_lab1.java
@@ -57,36 +57,7 @@ public class Java_lab1 {
     //*************************************************************************************************
     //*****Class, responsible for defining type of map and set of instructions for its generation******
     //*************************************************************************************************
-    private static class Director
-    {
-        //********************Creates random map*****************************************************
-        private void createDefaultMap(Builder builder)
-        {
-            builder.setRoads();
-            builder.setReservoirs();
-            builder.setBuildings();
-            builder.setLocalities();
-            builder.setTrees();
-            builder.setRelief();
-        }
-
-        //********************Creates map, customized by user****************************************
-        private void createUserMap(Builder builder)
-        {
-            System.out.println("Enter the number of roads: ");
-            builder.setRoads(input.nextInt());
-            System.out.println("Enter the number of reservoirs: ");
-            builder.setReservoirs(input.nextInt());
-            System.out.println("Enter the number of buildings: ");
-            builder.setBuildings(input.nextInt());
-            System.out.println("Enter the number of localities: ");
-            builder.setLocalities(input.nextInt());
-            System.out.println("Enter the number of trees: ");
-            builder.setTrees(input.nextInt());
-            System.out.println("Enter the number of relief: ");
-            builder.setRelief(input.nextInt());
-        }
-    }
+    
 
     //*************************************************************************************************
     //**************************Creates application****************************************************
