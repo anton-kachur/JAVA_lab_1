c144442f4925cb7466a8bf4daf9193adaed9667b
anton-kachur <40312443+anton-kachur@users.noreply.github.com>
2020-10-21 14:58:23 +0300

    Create Builder.java

---
+++ Builder.java
@@ -0,0 +1,15 @@
+public abstract class Builder
+    {
+        public abstract void setRoads();
+        public abstract void setReservoirs();
+        public abstract void setBuildings();
+        public abstract void setLocalities();
+        public abstract void setLocalities(int number_of_localities);
+        public abstract void setTrees();
+        public abstract void setRelief();
+        public abstract void setRoads(int n);
+        public abstract void setReservoirs(int n);
+        public abstract void setBuildings(int n);
+        public abstract void setTrees(int n);
+        public abstract void setRelief(int n);
+    }
