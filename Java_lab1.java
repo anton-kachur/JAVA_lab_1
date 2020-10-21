4fe4c21f60f3b28fc9a7cbc5082b9106cdd102f3
anton-kachur <40312443+anton-kachur@users.noreply.github.com>
2020-10-21 14:49:42 +0300

    Update Java_lab1.java

--- Java_lab1.java
+++ Java_lab1.java
@@ -33,21 +33,6 @@ public class Java_lab1 {
     //*************************************************************************************************
     //*******************All necessary classes*********************************************************
     //*************************************************************************************************
-   
-
-    public static class Building
-    {
-        private String name_of_building;
-        private String name_of_locality;
-
-        private Building(String name, String loc_name) {
-            name_of_building = name;
-            name_of_locality = loc_name;
-        }
-
-        public void printAll() { System.out.println("Name of building: " + name_of_building + "\nName of locality: " + name_of_locality); }
-    }
-
 
     public static class Locality extends Building
     {
