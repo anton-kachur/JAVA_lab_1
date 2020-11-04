08364c32f7b7629bfaf391896586f840e1bb128d
anton-kachur <40312443+anton-kachur@users.noreply.github.com>
2020-11-04 09:45:13 +0200

    Update Locality.java

--- Locality.java
+++ Locality.java
@@ -1,10 +1,11 @@
- public static class Locality extends Building
+public static class Locality extends Building
     {
         private String locality_name;
         private String locality_type;
 
-        private Locality(String name, String type) {
-            super("Default name", name);
+        private Locality(String name, String type, int n) {
+
+            super("Default name", name, n);
 
             locality_name = name;
             locality_type = type;
@@ -12,5 +13,16 @@
 
         private String getName() { return locality_name; }
 
-        public void printAll() { System.out.println("Name of locality: " + locality_name + "\nType of locality: " + locality_type); }
+        public void printAll() {
+            System.out.println("Name of locality: " + locality_name + "\nType of locality: " + locality_type);
+        }
+
+        public void printAll(boolean b) {
+            if (b)
+                System.out.println("Name of locality: " + locality_name + "\nType of locality: " + locality_type);
+            else {
+                System.out.println("Name of locality: " + locality_name + "\nType of locality: " + locality_type);
+                System.out.println("Name of building in locality: " + name_of_building + num_building);
+            }
+        }
     }
