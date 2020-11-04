8e97006d537b5f7b2bec118c94c6090e276e9382
anton-kachur <40312443+anton-kachur@users.noreply.github.com>
2020-11-04 09:43:46 +0200

    Update Map.java

--- Map.java
+++ Map.java
@@ -1,4 +1,4 @@
-private class Map
+private static class Map
     {
         //*************************************************************************************************
         //**********************Nested class, because each map has its own relief**************************
@@ -9,7 +9,8 @@ private class Map
             private int height;
 
             //*********Sets name of relief based on its height above the sea level***************************
-            private Relief(int h) {
+            private Relief(int h, String name_rel) {
+                name = name_rel;
                 height = h;
                 if (height > 0 && height < 200) name = relief_types[0];
                 else if (height > 200 && height < 500) name = relief_types[1];
@@ -23,7 +24,10 @@ private class Map
             }
 
         }
+
         //***********************ArrayLists which contain map components**************************************
+        private Scale scale;
+        private String timezone;
         private ArrayList<Road> roads = new ArrayList<>();
         private ArrayList<Reservoir> reservoirs = new ArrayList<>();
         private ArrayList<Building> buildings = new ArrayList<>();
@@ -44,56 +48,33 @@ private class Map
             }
         }
 
+        private void findDuplicates() {
+            if (localities.size() < 1) {
+                System.out.println("\nThere is no localities\n");
+            } else {
+                for (int i=1; i<localities.size(); i++) {
+                    if (localities.get(i-1).equals(localities.get(i))) {
+                        System.out.println("There are duplicates: "+localities.get(i-1));
+                    }
+                }
+
+            }
+        }
+
 
         //**************Methods for setting map components and representing them*****************************
         private void printMap() {
-            for (Road i: roads) i.printAll();
-            for (Reservoir i: reservoirs) i.printAll();
-            for (Building i: buildings) i.printAll();
-            for (Locality i: localities) i.printAll();
-            for (Trees i: trees) i.printAll();
-            for (Relief i: reliefs) i.printAll();
-        }
+            for (Road i : roads) i.printAll();
+            for (Reservoir i : reservoirs) i.printAll();
+            for (Building i : buildings) i.printAll();
 
-        private void setNewRoad(int amount) {
-            for (int i=0; i<amount; i++) {
-                Random random = new Random();
-                roads.add(new Road(road_types[random.nextInt(road_types.length)], random.nextInt(7)));
-            }
-        }
+            //With string concatenation with the other types
+            for (Locality i : localities) i.printAll();
+            //And not
+            for (Locality i : localities) i.printAll(false);
 
-        private void setNewReservoir(int amount) {
-            for (int i=0; i<amount; i++) {
-                Random random = new Random();
-                reservoirs.add(new Reservoir(reservoir_types[random.nextInt(reservoir_types.length)], random.nextInt(1000)));
-            }
+            for (Trees i : trees) i.printAll();
+            for (Relief i : reliefs) i.printAll();
+            System.out.println("Map scale: "+scale+" Timezone: "+timezone);
         }
-
-        private void setNewBuilding(int amount) {
-            for (int i=0; i<amount; i++) {
-                buildings.add(new Building("SomeBuilding", "SomeLocality"));
-            }
-        }
-
-        private void setNewLocality(int amount) {
-            for (int i=0; i<amount; i++) {
-                Random random = new Random();
-                localities.add(new Locality(generateName(), locality_types[random.nextInt(locality_types.length)]));
-            }
-        }
-
-        private void setNewTree(int amount) {
-            for (int i=0; i<amount; i++) {
-                Random random = new Random();
-                trees.add(new Trees("Something", trees_types[random.nextInt(trees_types.length)]));
-            }
-        }
-
-        private void setNewRelief(int amount) {
-            for (int i=0; i<amount; i++) {
-                Random random = new Random();
-                reliefs.add(new Relief(random.nextInt(5000)));
-            }
-        }
-
     }
